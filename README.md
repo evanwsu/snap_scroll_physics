7dabcb047863c31a57acce54c8fed4eed0176b31
Evan <wuzheng@dm-maker.com>
2023-06-28 17:16:31 +0800

    * * *初始化项目

---
+++ README.md
@@ -0,0 +1,31 @@
+## Features
+
+
+## Getting started
+
+TODO: List prerequisites and provide or point to information on how to
+start using the package.
+
+## Usage
+
+TODO: Include short and useful examples for package users. Add longer examples
+to `/example` folder.
+
+```dart
+// ListView
+ListView(
+physics: const SnapScrollPhysics(snapHeight: 250)
+);
+
+// CustomScrollView
+CustomScrollView(
+physics: const SnapScrollPhysics(snapHeight: 250)
+);
+
+```
+
+## Additional information
+
+TODO: Tell users more about the package: where to find more information, how to
+contribute to the package, how to file issues, what response they can expect
+from the package authors, and more.
---
+++ example/lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:snap_scroll_physics/snap_scroll_physics.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView(
+        physics: const SnapScrollPhysics(snapHeight: 250),
+        children: [
+          Container(
+            height: 250,
+            color: Colors.red,
+          ),
+          Container(
+            height: 200,
+            color: Colors.yellow,
+          ),
+          Container(
+            height: 800,
+            color: Colors.blue,
+          ),
+        ],
+      )
+    );
+  }
+}
